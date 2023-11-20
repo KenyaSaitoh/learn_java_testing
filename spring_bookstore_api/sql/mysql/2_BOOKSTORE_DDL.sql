f908771631b59430679c909b33196ebf6e75ac26
Kenya Saitoh <kenya.saitoh@gmail.com>
2023-11-21 08:51:33 +0900

    commit

--- spring_bookstore_api/sql/mysql/2_BOOKSTORE_DDL.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE CUSTOMER (
-CUSTOMER_ID   INT AUTO_INCREMENT PRIMARY KEY, -- 顧客ID
-CUSTOMER_NAME VARCHAR(30) NOT NULL,           -- 顧客名
-PASSWORD      VARCHAR(60) NOT NULL,           -- パスワード
-EMAIL         VARCHAR(30) NOT NULL,           -- Eメールアドレス
-BIRTHDAY      DATE,                           -- 生年月日
-ADDRESS       VARCHAR(120),                   -- 住所（UTF-8で40文字）
-CONSTRAINT EMAIL_UNIQUE UNIQUE(EMAIL)
-);
--- spring_bookstore_web/src/main/java/pro/kensait/spring/bookstore/web/book/SearchParam.java
+++
@@ -1,6 +0,0 @@
-package pro.kensait.spring.bookstore.web.book;
-
-public record SearchParam(
-        Integer categoryId,
-        String keyword) {
-}
