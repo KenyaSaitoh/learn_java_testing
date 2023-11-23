79e0871711473d26b04949ff9ea2604853facb70
Kenya Saitoh <kenya.saitoh@gmail.com>
2023-11-23 21:59:44 +0900

    commit

---
+++ 04_mockito_basic/src/test/java/pro/kensait/mockito/basic/MockitoTest.java
@@ -0,0 +1,25 @@
+package pro.kensait.mockito.basic;
+
+import static org.mockito.Mockito.*;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+public class MockitoTest {
+
+    @SuppressWarnings("unchecked")
+    @Test
+    void test01() {
+        Map<Integer, String> mock = mock(Map.class);
+
+        when(mock.put(0, "hoge"));
+        when(mock.get(0)).thenReturn("foo");
+        when(mock.get(1)).thenReturn("bar");
+        when(mock.get(2)).thenReturn("baz");
+        
+        System.out.println(mock.get(0));
+        System.out.println(mock.get(1));
+        System.out.println(mock.get(2));
+    }
+}
