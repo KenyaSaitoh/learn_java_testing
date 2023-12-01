cc5b9b6385f7d142128e7ca92805ea404314a4e1
Kenya Saitoh <kenya.saitoh@gmail.com>
2023-12-01 22:09:37 +0900

    commit

--- junit5_dependency/src/test/java/pro/kensait/java/shipping/ShippingServiceTest.java
+++ junit5_dependency/src/test/java/pro/kensait/java/shipping/ShippingServiceTest.java
@@ -20,19 +20,24 @@ public class ShippingServiceTest {
     // テスト対象クラス
     ShippingService shippingService;
 
-    // 共通的な変数
+    // 共通的な変数（テスト対象クラスの引数）
     LocalDate receiveDate;
     Client diamondClient;
     Client goldClient;
 
     // 共通的な変数のうち、モック対象
-    MockCostCalculator mockCostCalculator;
+    MockCostCalculator costCalculator;
 
     // 変数を初期化する
     @BeforeEach
     void setUp() {
-        mockCostCalculator = new MockCostCalculator();
-        shippingService = new ShippingService(mockCostCalculator);
+        // モックを生成する
+        costCalculator = new MockCostCalculator();
+
+        // モックをテスト対象クラスに注入する
+        shippingService = new ShippingService(costCalculator);
+
+        // 変数を設定する
         diamondClient = new Client(10001, "Alice", "大阪府住吉区1-1-1",
                 ClientType.DIAMOND, RegionType.KANSAI);
         goldClient = new Client(10001, "Alice", "大阪府住吉区1-1-1",
@@ -43,72 +48,12 @@ public class ShippingServiceTest {
         ShippingRepository.shippingList.clear();
     }
 
-    @Test
-    void testOrderShipping_DiamondCustomer_NoDiscount() {
-        // テスト用の顧客と荷物リストを設定する
-        List<Baggage> baggageList = Arrays.asList(
-                new Baggage(BaggageType.MIDDLE, false));
-
-        // テスト実行
-        shippingService.orderShipping(diamondClient, receiveDate, baggageList);
-
-        // リポジトリから「実際の値」を取得する
-        Shipping actual = ShippingRepository.shippingList.get(0);
-
-        // 「期待値」を生成する
-        Shipping expected = new Shipping(LocalDate.now(), diamondClient, receiveDate, baggageList,
-                1600);
-
-        // 「期待値」と「実際の値」が一致しているかを検証する
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void testOrderShipping_DiamondCustomer_Discount_ReachLimit() {
-        // 荷物リストを設定する
-        List<Baggage> baggageList = Arrays.asList(
-                new Baggage(BaggageType.MIDDLE, false),
-                new Baggage(BaggageType.LARGE, true));
-
-        // テスト実行
-        shippingService.orderShipping(diamondClient, receiveDate, baggageList);
-
-        // リポジトリから「実際の値」を取得する
-        Shipping actual = ShippingRepository.shippingList.get(0);
-
-        // 「期待値」を生成する
-        Shipping expected = new Shipping(LocalDate.now(), diamondClient, receiveDate, baggageList,
-                2500);
-
-        // 「期待値」と「実際の値」が一致しているかを検証する
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void testOrderShipping_DiamondCustomer_Discount_NoLimit() {
-        // 荷物リストを設定する
-        List<Baggage> baggageList = Arrays.asList(
-                new Baggage(BaggageType.SMALL, false),
-                new Baggage(BaggageType.MIDDLE, true),
-                new Baggage(BaggageType.LARGE, true));
-
-        // テスト実行
-        shippingService.orderShipping(diamondClient, receiveDate, baggageList);
-
-        // リポジトリから「実際の値」を取得する
-        Shipping actual = ShippingRepository.shippingList.get(0);
-
-        // 「期待値」を生成する
-        Shipping expected = new Shipping(LocalDate.now(), diamondClient, receiveDate, baggageList,
-                3600);
-
-        // 「期待値」と「実際の値」が一致しているかを検証する
-        assertEquals(expected, actual);
-    }
-
+    /*
+     * ゴールド会員で、割引なしになった場合の更新結果をテストする
+     */
     @Test
     void testOrderShipping_GoldCustomer_NoDiscount() {
-        // テスト用の顧客と荷物リストを設定する
+        // 引数である荷物リストを生成する（テスト毎に異なる）
         List<Baggage> baggageList = Arrays.asList(
                 new Baggage(BaggageType.MIDDLE, false));
 
@@ -126,9 +71,12 @@ public class ShippingServiceTest {
         assertEquals(expected, actual);
     }
 
+    /*
+     * ゴールド会員で、割引になった場合（ただし下限に到達）の更新結果をテストする
+     */
     @Test
     void testOrderShipping_GoldCustomer_Discount_ReachLimit() {
-        // 荷物リストを設定する
+        // 引数である荷物リストを生成する（テスト毎に異なる）
         List<Baggage> baggageList = Arrays.asList(
                 new Baggage(BaggageType.MIDDLE, false),
                 new Baggage(BaggageType.LARGE, true));
@@ -147,9 +95,12 @@ public class ShippingServiceTest {
         assertEquals(expected, actual);
     }
 
+    /*
+     * ゴールド会員で、割引になった場合（下限に到達せず）の更新結果をテストする
+     */
     @Test
     void testOrderShipping_GoldCustomer_Discount_NoLimit() {
-        // 荷物リストを設定する
+        // 引数である荷物リストを生成する（テスト毎に異なる）
         List<Baggage> baggageList = Arrays.asList(
                 new Baggage(BaggageType.SMALL, false),
                 new Baggage(BaggageType.MIDDLE, true),
@@ -164,6 +115,77 @@ public class ShippingServiceTest {
         // 「期待値」を生成する
         Shipping expected = new Shipping(LocalDate.now(), goldClient, receiveDate, baggageList,
                 4320);
+
+        // 「期待値」と「実際の値」が一致しているかを検証する
+        assertEquals(expected, actual);
+    }
+
+    /*
+     * ダイヤモンド会員で、割引なしになった場合の更新結果をテストする
+     */
+    @Test
+    void testOrderShipping_DiamondCustomer_NoDiscount() {
+        // 引数である荷物リストを生成する（テスト毎に異なる）
+        List<Baggage> baggageList = List.of(new Baggage(BaggageType.MIDDLE, false));
+
+        // テスト実行
+        shippingService.orderShipping(diamondClient, receiveDate, baggageList);
+
+        // リポジトリから「実際の値」を取得する
+        Shipping actual = ShippingRepository.shippingList.get(0);
+
+        // 「期待値」を生成する
+        Shipping expected = new Shipping(LocalDate.now(), diamondClient, receiveDate, baggageList,
+                1600);
+
+        // 「期待値」と「実際の値」が一致しているかを検証する
+        assertEquals(expected, actual);
+    }
+
+    /*
+     * ダイヤモンド会員で、割引になった場合（ただし下限に到達）の更新結果をテストする
+     */
+    @Test
+    void testOrderShipping_DiamondCustomer_Discount_ReachLimit() {
+        // 引数である荷物リストを生成する（テスト毎に異なる）
+        List<Baggage> baggageList = Arrays.asList(
+                new Baggage(BaggageType.MIDDLE, false),
+                new Baggage(BaggageType.LARGE, true));
+
+        // テスト実行
+        shippingService.orderShipping(diamondClient, receiveDate, baggageList);
+
+        // リポジトリから「実際の値」を取得する
+        Shipping actual = ShippingRepository.shippingList.get(0);
+
+        // 「期待値」を生成する
+        Shipping expected = new Shipping(LocalDate.now(), diamondClient, receiveDate, baggageList,
+                2500);
+
+        // 「期待値」と「実際の値」が一致しているかを検証する
+        assertEquals(expected, actual);
+    }
+
+    /*
+     * ダイヤモンド会員で、割引になった場合（下限に到達せず）の更新結果をテストする
+     */
+    @Test
+    void testOrderShipping_DiamondCustomer_Discount_NoLimit() {
+        // 引数である荷物リストを生成する（テスト毎に異なる）
+        List<Baggage> baggageList = Arrays.asList(
+                new Baggage(BaggageType.SMALL, false),
+                new Baggage(BaggageType.MIDDLE, true),
+                new Baggage(BaggageType.LARGE, true));
+
+        // テスト実行
+        shippingService.orderShipping(diamondClient, receiveDate, baggageList);
+
+        // リポジトリから「実際の値」を取得する
+        Shipping actual = ShippingRepository.shippingList.get(0);
+
+        // 「期待値」を生成する
+        Shipping expected = new Shipping(LocalDate.now(), diamondClient, receiveDate, baggageList,
+                3600);
 
         // 「期待値」と「実際の値」が一致しているかを検証する
         assertEquals(expected, actual);
--- mockito_junit5/src/test/java/pro/kensait/java/shipping/ShippingServiceTest.java
+++ mockito_junit5/src/test/java/pro/kensait/java/shipping/ShippingServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -22,12 +23,13 @@ public class ShippingServiceTest {
     // テスト対象クラス
     ShippingService shippingService;
 
-    // 共通的な変数
+    // 共通的な変数（テスト対象クラスの引数）
     LocalDate receiveDate;
+    Client diamondClient;
+    Client goldClient;
 
     // 共通的な変数のうち、モック対象（@Mockを付与）
     @Mock CostCalculatorIF costCalculator;
-    @Mock Client client; // ???
 
     // 変数を初期化する
     @BeforeEach
@@ -35,36 +37,182 @@ public class ShippingServiceTest {
         // モックを初期化する（@Mockが付与されたフィールドにモックを割り当てる）
         MockitoAnnotations.openMocks(this);
 
-
+        // モックをテスト対象クラスに注入する
         shippingService = new ShippingService(costCalculator);
+
+        // 変数を設定する
+        diamondClient = new Client(10001, "Alice", "大阪府住吉区1-1-1",
+                ClientType.DIAMOND, RegionType.KANSAI);
+        goldClient = new Client(10001, "Alice", "大阪府住吉区1-1-1",
+                ClientType.GOLD, RegionType.KANSAI);
         receiveDate = LocalDate.of(2023, 11, 30);
 
         // リポジトリをクリアする → これはデータベース初期化に相当する
         ShippingRepository.shippingList.clear();
     }
 
+    
+    /*
+     * ゴールド会員で、割引なしになった場合の更新結果をテストする
+     */
+    @Test
+    void testOrderShipping_GoldCustomer_NoDiscount() {
+        // モック化されたcostCalculatorの振る舞いを決める
+        when(costCalculator.calcShippingCost(
+                BaggageType.MIDDLE, RegionType.KANSAI)).thenReturn(1600);
+
+        // 引数である荷物リストを生成する（テスト毎に異なる）
+        List<Baggage> baggageList = List.of(new Baggage(BaggageType.MIDDLE, false));
+
+        // テスト実行
+        shippingService.orderShipping(goldClient, receiveDate, baggageList);
+
+        // リポジトリから「実際の値」を取得する
+        Shipping actual = ShippingRepository.shippingList.get(0);
+
+        // 「期待値」を生成する
+        Shipping expected = new Shipping(LocalDate.now(), goldClient, receiveDate, baggageList,
+                1600);
+
+        // 「期待値」と「実際の値」が一致しているかを検証する
+        assertEquals(expected, actual);
+    }
+
+    /*
+     * ゴールド会員で、割引になった場合（ただし下限に到達）の更新結果をテストする
+     */
+    @Test
+    void testOrderShipping_GoldCustomer_Discount_ReachLimit() {
+        // モック化されたcostCalculatorの振る舞いを決める
+        when(costCalculator.calcShippingCost(
+                BaggageType.MIDDLE, RegionType.KANSAI)).thenReturn(1600);
+
+        // 引数である荷物リストを生成する（テスト毎に異なる）
+        List<Baggage> baggageList = Arrays.asList(
+                new Baggage(BaggageType.MIDDLE, false),
+                new Baggage(BaggageType.MIDDLE, false));
+
+        // テスト実行
+        shippingService.orderShipping(goldClient, receiveDate, baggageList);
+
+        // リポジトリから「実際の値」を取得する
+        Shipping actual = ShippingRepository.shippingList.get(0);
+
+        // 「期待値」を生成する
+        Shipping expected = new Shipping(LocalDate.now(), goldClient, receiveDate, baggageList,
+                3000);
+
+        // 「期待値」と「実際の値」が一致しているかを検証する
+        assertEquals(expected, actual);
+    }
+
+    /*
+     * ゴールド会員で、割引になった場合（下限に到達せず）の更新結果をテストする
+     */
+    @Test
+    void testOrderShipping_GoldCustomer_Discount_NoLimit() {
+        // モック化されたcostCalculatorの振る舞いを決める
+        when(costCalculator.calcShippingCost(
+                BaggageType.MIDDLE, RegionType.KANSAI)).thenReturn(1600);
+
+        // 引数である荷物リストを生成する（テスト毎に異なる）
+        List<Baggage> baggageList = Arrays.asList(
+                new Baggage(BaggageType.MIDDLE, false),
+                new Baggage(BaggageType.MIDDLE, false),
+                new Baggage(BaggageType.MIDDLE, false));
+
+        // テスト実行
+        shippingService.orderShipping(goldClient, receiveDate, baggageList);
+
+        // リポジトリから「実際の値」を取得する
+        Shipping actual = ShippingRepository.shippingList.get(0);
+
+        // 「期待値」を生成する
+        Shipping expected = new Shipping(LocalDate.now(), goldClient, receiveDate, baggageList,
+                4320);
+
+        // 「期待値」と「実際の値」が一致しているかを検証する
+        assertEquals(expected, actual);
+    }
+
+    /*
+     * ダイヤモンド会員で、割引なしになった場合の更新結果をテストする
+     */
     @Test
     void testOrderShipping_DiamondCustomer_NoDiscount() {
         // モック化されたcostCalculatorの振る舞いを決める
         when(costCalculator.calcShippingCost(
                 BaggageType.MIDDLE, RegionType.KANSAI)).thenReturn(1600);
 
-        // モック化されたclientの振る舞いを決める
-        // 引数なのでモックか不要かも！
-        when(client.ClientType()).thenReturn(ClientType.DIAMOND);
-        when(client.originRegion()).thenReturn(RegionType.KANSAI);
-
+        // 引数である荷物リストを生成する（テスト毎に異なる）
         List<Baggage> baggageList = List.of(new Baggage(BaggageType.MIDDLE, false));
 
         // テスト実行
-        shippingService.orderShipping(client, receiveDate, baggageList);
+        shippingService.orderShipping(diamondClient, receiveDate, baggageList);
 
         // リポジトリから「実際の値」を取得する
         Shipping actual = ShippingRepository.shippingList.get(0);
 
         // 「期待値」を生成する
-        Shipping expected = new Shipping(LocalDate.now(), client, receiveDate, baggageList,
+        Shipping expected = new Shipping(LocalDate.now(), diamondClient, receiveDate, baggageList,
                 1600);
+
+        // 「期待値」と「実際の値」が一致しているかを検証する
+        assertEquals(expected, actual);
+    }
+
+    /*
+     * ダイヤモンド会員で、割引になった場合（ただし下限に到達）の更新結果をテストする
+     */
+    @Test
+    void testOrderShipping_DiamondCustomer_Discount_ReachLimit() {
+        // モック化されたcostCalculatorの振る舞いを決める
+        when(costCalculator.calcShippingCost(
+                BaggageType.MIDDLE, RegionType.KANSAI)).thenReturn(1600);
+
+        // 引数である荷物リストを生成する（テスト毎に異なる）
+        List<Baggage> baggageList = Arrays.asList(
+                new Baggage(BaggageType.MIDDLE, false),
+                new Baggage(BaggageType.MIDDLE, false));
+
+        // テスト実行
+        shippingService.orderShipping(diamondClient, receiveDate, baggageList);
+
+        // リポジトリから「実際の値」を取得する
+        Shipping actual = ShippingRepository.shippingList.get(0);
+
+        // 「期待値」を生成する
+        Shipping expected = new Shipping(LocalDate.now(), diamondClient, receiveDate, baggageList,
+                2500);
+
+        // 「期待値」と「実際の値」が一致しているかを検証する
+        assertEquals(expected, actual);
+    }
+
+    /*
+     * ダイヤモンド会員で、割引になった場合（下限に到達せず）の更新結果をテストする
+     */
+    @Test
+    void testOrderShipping_DiamondCustomer_Discount_NoLimit() {
+        // モック化されたcostCalculatorの振る舞いを決める
+        when(costCalculator.calcShippingCost(
+                BaggageType.MIDDLE, RegionType.KANSAI)).thenReturn(1600);
+
+        // 引数である荷物リストを生成する（テスト毎に異なる）
+        List<Baggage> baggageList = Arrays.asList(
+                new Baggage(BaggageType.MIDDLE, false),
+                new Baggage(BaggageType.MIDDLE, false),
+                new Baggage(BaggageType.MIDDLE, false));
+
+        // テスト実行
+        shippingService.orderShipping(diamondClient, receiveDate, baggageList);
+
+        // リポジトリから「実際の値」を取得する
+        Shipping actual = ShippingRepository.shippingList.get(0);
+
+        // 「期待値」を生成する
+        Shipping expected = new Shipping(LocalDate.now(), diamondClient, receiveDate, baggageList,
+                3600);
 
         // 「期待値」と「実際の値」が一致しているかを検証する
         assertEquals(expected, actual);
