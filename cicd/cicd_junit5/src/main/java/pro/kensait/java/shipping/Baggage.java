71991402244de203edbdbfc56ac392418b2801ed
Kenya Saitoh <kenya.saitoh@gmail.com>
2023-12-12 20:32:48 +0900

    commit

--- cicd/cicd_junit5/src/main/java/pro/kensait/java/shipping/Baggage.java
+++
@@ -1,9 +0,0 @@
-package pro.kensait.java.shipping;
-
-/*
- * 荷物データ
- */
-public record Baggage (
-    BaggageType baggageType,
-    boolean isFragile) {
-}
--- cicd/cicd_junit5/src/main/java/pro/kensait/java/shipping/ClientType.java
+++
@@ -1,8 +0,0 @@
-package pro.kensait.java.shipping;
-
-/*
- * クライアント種別
- */
-public enum ClientType {
-    GENERAL, GOLD, DIAMOND;
-}
